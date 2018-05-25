e2771e9b07135d89df122c464506bcdee59e2ce5
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-05-25 14:39:26 -0300

    fix missing semicolon

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,11 @@
-const { readFile } = require('fs')
+const { readFile } = require('fs');
 
 const getFileContent = filePath => new Promise((resolve, reject) => {
   readFile(filePath, (error, content) => {
-    error ? reject(error) : resolve(content)
-  })
-})
+    error ? reject(error) : resolve(content);
+  });
+});
 
 module.exports = {
-  getFileContent
+  getFileContent,
 };
