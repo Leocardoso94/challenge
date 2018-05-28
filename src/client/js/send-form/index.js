3c83fab5a2f6919e557eb67ef4ada81a411bb5ea
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-28 19:57:45 -0300

    waiting for cep validation

--- src/client/js/send-form/index.js
+++ src/client/js/send-form/index.js
@@ -14,12 +14,12 @@ const sendData = () => {
   console.log(getDataFromForm());
 };
 
-export default () => {
+export default async () => {
   const fieldSet = document.querySelector('fieldset.user');
   showRequiredFieldErrorsOnFieldSet(fieldSet);
 
   validateEmail(document.querySelector('[type="email"]'));
-  validateCEP(document.querySelector('[type="cep"]'));
+  await validateCEP(document.querySelector('[type="cep"]'));
   validatePhoneNumber(document.querySelector('[type="phone"]'));
 
   if (hasAnErrorMessage()) sendData();
