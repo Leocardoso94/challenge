52b6f81e91e9444115c362e10b96ac635aa0fb76
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-28 16:21:18 -0300

    adding mask

--- src/client/index.js
+++ src/client/index.js
@@ -2,13 +2,26 @@ import generateForm from './js/generate-form';
 import './css/style.css';
 import changeTabs from './js/change-tabs';
 import sendForm from './js/send-form';
+import { mask } from './js/utils';
 
-generateForm();
 
-const buttonFirstStep = document.querySelector('.button.request-fields');
+(async () => {
+  const buttonFirstStep = document.querySelector('.button.request-fields');
+  buttonFirstStep.addEventListener('click', changeTabs);
 
-buttonFirstStep.addEventListener('click', changeTabs);
+  const buttonFinalize = document.querySelector('.button.user');
+  buttonFinalize.addEventListener('click', sendForm);
 
-const buttonFinalize = document.querySelector('.button.user');
+  await generateForm();
 
-buttonFinalize.addEventListener('click', sendForm);
+  const cepInput = document.querySelector('[type="cep"]');
+  cepInput.maxLength = 9;
+  cepInput.mask = '#####-###';
+  cepInput.addEventListener('keypress', mask);
+
+  const phoneInput = document.querySelector('[type="phone"]');
+  phoneInput.maxLength = 13;
+  phoneInput.mask = '## #####-####';
+
+  phoneInput.addEventListener('keypress', mask);
+})();
