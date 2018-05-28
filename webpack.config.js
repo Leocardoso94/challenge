3159ce229953eb3d7f02e3f4884aa3512f9cbe70
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-28 15:19:54 -0300

    removing babel plugins

--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
         test: /\.html$/,
         use: [
           {
