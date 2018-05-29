c01d27108fa90c8a98a87a6e9f2083d68553cfd6
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-28 23:22:10 -0300

    refactoring

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,12 @@ import changeTabs from './js/change-tabs';
 import sendForm from './js/send-form';
 import { mask } from './js/utils';
 
+const maskField = (selector, maxLength, maskFormat) => {
+  const input = document.querySelector(selector);
+  input.maxLength = maxLength;
+  input.mask = maskFormat;
+  input.addEventListener('keypress', mask);
+};
 
 (async () => {
   const buttonFirstStep = document.querySelector('.button.request-fields');
@@ -14,14 +20,6 @@ import { mask } from './js/utils';
 
   await generateForm();
 
-  const cepInput = document.querySelector('[type="cep"]');
-  cepInput.maxLength = 9;
-  cepInput.mask = '#####-###';
-  cepInput.addEventListener('keypress', mask);
-
-  const phoneInput = document.querySelector('[type="phone"]');
-  phoneInput.maxLength = 13;
-  phoneInput.mask = '## #####-####';
-
-  phoneInput.addEventListener('keypress', mask);
+  maskField('[type="cep"]', 9, '#####-###');
+  maskField('[type="phone"]', 13, '## #####-####');
 })();
